978d9413002c34f935d8234cfc1bb7b6097ed516
pau <a14pauagurad@inspedralbes.cat>
2019-11-12 16:10:13 +0100

    V2

--- afegeix-usuari-local.sh
+++ afegeix-usuari-local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #discrimina root de noroot
 if [[ "${UID}" -eq 0 ]]
 then
@@ -7,13 +8,26 @@ then
 	read -p "Primer nombre del usuario: " fnuser
 	read -p "Contraseña: " password
 	#Creació del usuari i assigna contrasse
-	useradd -c "${fnuser}" -m ${username} 
+	useradd -c "${fnuser}" -m ${username}
+	c_usu=$(cat /etc/passwd | grep ${username}) 
 	#Comprova usuari
+	if [[ "${c_usu}"="${username}" ]]
+	then
+		echo "Usuari creat correctament"
+	else 
+		exit 2
+	fi
 	cat /etc/passwd | grep ${username}
 	#Dona pass
 	echo "${username}:${password}" | chpasswd
+	c_pass=$(cat /etc/shadow | grep ${username})
 	#comprova pass
-	cat /etc/shadow | grep ${username} 
+	if [[ "$username"="$c_pass" ]]
+	then
+		echo "Contrassenya afegida correctament"
+	else
+		exit 2
+	fi
 	#Obliga a cambiar pass en el proper login
 	passwd -e ${username}
 	#Mostra les dades del usuari
